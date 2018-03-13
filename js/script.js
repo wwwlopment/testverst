d97cd628292571b641ac10fb4e290b310495e382
Doctor777 <aptekasbt@gmail.com>
2018-03-13 16:10:35 +0200

    added_sliders

--- js/script.js
+++ js/script.js
@@ -1,29 +1,9 @@
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-
 // ===== Scroll to Top ====
 $(window).scroll(function() {
-    if ($(this).scrollTop() >= 1200) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(500);    // Fade in the arrow
+    if ($(this).scrollTop() >= 100) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(100);    // Fade in the arrow
     } else {
-        $('#return-to-top').fadeOut(500);   // Else fade out the arrow
+        $('#return-to-top').fadeOut(100);   // Else fade out the arrow
     }
 });
 $('#return-to-top').click(function() {      // When arrow is clicked
@@ -31,3 +11,4 @@ $('#return-to-top').click(function() {      // When arrow is clicked
         scrollTop : 0                       // Scroll to top of body
     }, 500);
 });
+
